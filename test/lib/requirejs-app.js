ce042d7dc83cb920c18a7d75d07ce1606850ee21
刘小月 <xiaoyue@liuxiaoyuedeMacBook-Pro.local>
2015-01-23 13:40:00 +0800

    support dir or file  assert test

--- test/lib/requirejs-app.js
+++ test/lib/requirejs-app.js
@@ -1,7 +1,7 @@
-define(['passing'], function(require) {
-  if (window.mochaPhantomJS) {
-    return mochaPhantomJS.run();
-  } else {
-    return mocha.run();
-  }
-});
+// define(['passing'], function(require) {
+//   if (window.mochaPhantomJS) {
+//     return mochaPhantomJS.run();
+//   } else {
+//     return mocha.run();
+//   }
+// });
